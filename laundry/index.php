9d277c195bb53125347d34fe5c502b2180618b1c
XIRPL2_CHOIRUL-blip <pentolngapungdiudara@gmail.com>
2026-09-22 09:48:48 +0700

    laundry

---
+++ laundry/index.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>Sistem Informasi Laundry</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+        <script type="text/javascript" src="assets/js/jquery.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    </head>
+    <body style="background: #fdfcfc;">
+        <br></br>
+        <center>
+            <h2>SISTEM INFORMASI LAUNDRY</h2>
+        </center>
+        <br></br>
+
+        <div class="container">
+            <div class="col-md-4 col-md-offset-4">
+                <form>
+                    <div class="panel">
+                        <br>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <label>Username</label>
+                                <input type="text" name="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="Password" name="password" class="form-control">
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Log In">
+                        </div>
+                        <br>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
